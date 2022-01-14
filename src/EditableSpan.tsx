8fcf0f3adb9ac7a957a4abecc8bea71d6a0d6f72
jiemoh <jiemoh@list.ru>
2022-01-14 23:27:28 +0300

    refactoring

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -23,6 +23,6 @@ export function EditableSpan(props: EditableSpanPropsType) {
     }
 
     return editMode
-        ? <TextField variant={"outlined"} value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
+        ? <TextField variant={"standard"} value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
         : <span onDoubleClick={activateEditMode}>{props.value}</span>
 }
