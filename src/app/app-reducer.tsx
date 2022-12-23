09ddc0046442d2f893ffde7d7aed93e593453544
jiemoh <jiemoh@list.ru>
2022-12-24 02:37:15 +0300

    added Preloader

---
+++ src/app/app-reducer.tsx
@@ -0,0 +1,23 @@
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+type ErrorType = string | null
+
+const initialState = {
+    status: 'loading' as RequestStatusType,
+    error: null as ErrorType
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: AppActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
+        default:
+            return state
+    }
+}
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
+export type AppActionsType = ReturnType<typeof setAppStatusAC> | ReturnType<typeof setAppErrorAC>
